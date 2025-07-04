5fbfcbe2269859cca4f91d4706414876e51bfeec
maikercanon <maikersito200109@gmail.com>
2025-07-04 14:02:14 -0500

    correccion vercel

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,3 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import { AppComponent } from './app/app.component';
-
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
 import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
@@ -11,6 +5,7 @@ import { AppComponent } from './app/app.component';
 import { PersonajeDetalleComponent } from './app/components/personaje-detalle/personaje-detalle.component';
 import { PersonajeListaComponent } from './app/components/personaje-lista/personaje-lista.component';
 import { ReportesComponent } from './app/components/reportes/reportes.component';
+
 const routes: Routes = [
   { path: '', component: PersonajeListaComponent },
   { path: 'personaje/:id', component: PersonajeDetalleComponent },
